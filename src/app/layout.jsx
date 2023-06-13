c1982ab12721219124eaa32feed3776c395639e0
Alexander <ajn.janssen@gmail.com>
2023-06-13 17:18:23 +0200

    test

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,10 +5,10 @@ import { Inter } from 'next/font/google'
 // import MobileNavigation from '../components/ui/navigation/MobileNavigation'
 // import DesktopNavigation from '../components/ui/navigation/DesktopNavigation'
 // import Window from '../components/ui/messageWindow/Window'
-// import BottomNav from '@/components/ui/navigation/BottomNav'
-// import MobileNavigation from '@/components/ui/navigation/MobileNavigation'
-// import DesktopNavigation from '@/components/ui/navigation/DesktopNavigation'
-// import Window from '@/components/ui/messageWindow/Window'
+import BottomNav from '@/components/ui/navigation/BottomNav'
+import MobileNavigation from '@/components/ui/navigation/MobileNavigation'
+import DesktopNavigation from '@/components/ui/navigation/DesktopNavigation'
+import Window from '@/components/ui/messageWindow/Window'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -26,26 +26,26 @@ export default function RootLayout({ children }) {
       </head>
       <body className={inter.className}>
         <div className="bg-base-100 flex no-scrollbar">
-          {/* <div className="z-20 top-0 sticky pb-16 md:w-1/6">
+          <div className="z-20 top-0 sticky pb-16 md:w-1/6">
             <div className="hidden lg:block">
               <DesktopNavigation />
             </div>
             <div className="lg:hidden">
               <MobileNavigation />
             </div>
-          </div> */}
+          </div>
 
           <div className="relative w-full lg:w-4/6 h-full lg:flex justify-center py-20 lg:py-20 px-4 lg:px-24 mx-auto no-scrollbar">
             <div className="w-full no-scrollbar">{children}</div>
           </div>
 
-          {/* <div className="mr-0 h-full hidden w-1/6 lg:block top-0 sticky">
+          <div className="mr-0 h-full hidden w-1/6 lg:block top-0 sticky">
             <Window />
           </div>
 
           <div className="lg:hidden">
             <BottomNav />
-          </div> */}
+          </div>
         </div>
       </body>
     </html>
