19637b31f01b323ed10686c633817258e97e6fe8
Alexander <ajn.janssen@gmail.com>
2023-06-13 17:59:47 +0200

    test

--- src/components/ui/messageWindow/Notifications.jsx
+++ src/components/ui/messageWindow/Notifications.jsx
@@ -1,23 +1,23 @@
 'use client'
 import React from 'react'
 function Notification({ image, title, date, type, description }) {
-  // const calculateTimeAgo = (date) => {
-  //   const currentDate = new Date()
-  //   const targetDate = new Date(date)
+  const calculateTimeAgo = (date) => {
+    const currentDate = new Date()
+    const targetDate = new Date(date)
 
-  //   const timeDiff = Math.abs(currentDate - targetDate)
-  //   const hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60))
-  //   const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24))
+    const timeDiff = Math.abs(currentDate - targetDate)
+    const hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60))
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24))
 
-  //   if (hoursDiff < 24) {
-  //     return `${hoursDiff} hours ago`
-  //   } else {
-  //     return `${daysDiff} days ago`
-  //   }
-  // }
+    if (hoursDiff < 24) {
+      return `${hoursDiff} hours ago`
+    } else {
+      return `${daysDiff} days ago`
+    }
+  }
   return (
     <button className="w-full flex text-left py-2 focus:outline-none focus-visible:bg-indigo-50">
-      {/* <div className="flex items-center">
+      <div className="flex items-center">
         <div className="grid place-items-center">
           <img
             className="rounded-full items-start flex-shrink-0 mr-3"
@@ -40,7 +40,7 @@ function Notification({ image, title, date, type, description }) {
             {calculateTimeAgo(date)} geleden
           </span>
         </div>
-      </div> */}
+      </div>
     </button>
   )
 }
